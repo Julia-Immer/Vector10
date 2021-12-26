a4ee4b0104e5fe0d6c177d5a5cf733e2b1afdf65
Julia Immer Scott <92125966+Julia-Immer@users.noreply.github.com>
2021-12-26 14:50:32 -0700

    Add files via upload

--- code/Vector10.cpp
+++ code/Vector10.cpp
@@ -1,7 +1,12 @@
 #include "Vector10.h"
 
 Vector10::Vector10(){
-    // YOUR CODE HERE
+    vect_back = 0;
+    //set all the values to zero for the vector
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i] = 0;
+    }
 }
 
 Vector10::~Vector10(){
@@ -10,22 +15,65 @@ Vector10::~Vector10(){
 
 // Implement this function first, since all tests use it.
 int Vector10::ValueAt(int index){
-    // YOUR CODE HERE
+
+    if ((index > vect_back) || (index < -10))
+    {
+        return 0;
+    }
+    return arr[index];
 }
 
 // Implement this function second, since most tests use it.
 bool Vector10::PushBack(int value){
-    // YOUR CODE HERE
+
+    if (vect_back > 9) 
+    {
+        return false;
+    }
+    else
+    {
+        //set value to first open space in arr
+        arr[vect_back] = value;
+        vect_back++;
+        return true;
+    }
 }
 
 int Vector10::CountEmpty(){
-    // YOUR CODE HERE
+    //where we'll store our count of the empty (0) elemenets
+    int empties = 10 - vect_back;
+
+    return empties;
 }
 
 bool Vector10::Search(int value){
-    // YOUR CODE HERE
+    for (int i = 0; i < vect_back; i++)
+    {
+        if (arr[i] == value)
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
 
+
 bool Vector10::Remove(int index){
-    // YOUR CODE HERE
+    //if it's out of bounds then we return false    
+    if (index > 9)
+    {
+        return false;
+    }
+    else if (index == vect_back)
+    {
+        return false;
+    }
+
+    for (int i = index; i < vect_back; i++)
+    {
+        arr[index] = arr[index + 1];
+        vect_back--;
+    }
+    return true;
 }
